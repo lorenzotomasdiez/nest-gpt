e57ceb2f1f445f55a0854ca084f36aa60506e8e8
Lorenzo Tomas Diez <lorenzotomasdiez@Lorenzos-MacBook-Air.local>
2024-06-06 00:04:41 -0300

    last changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,30 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
-import { GptModule } from './gpt/gpt.module';
+import { GraphQLModule } from '@nestjs/graphql';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GptModule } from './gpt/gpt.module';
 import { SamAssitantModule } from './sam-assitant/sam-assitant.module';
 import { TelegramModule } from './telegram/telegram.module';
-import { ScheduleModule } from '@nestjs/schedule';
 import { SchedulerModule } from './scheduler/scheduler.module';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
     ConfigModule.forRoot(),
     ScheduleModule.forRoot(),
     GptModule,
     SamAssitantModule,
     TelegramModule,
     SchedulerModule,
+    TodoModule,
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/todo/todo.resolver.ts
@@ -0,0 +1,34 @@
+import { Query, Resolver, Int, Args } from '@nestjs/graphql';
+import { Todo } from './entity/todo.entity';
+import { TodoService } from './todo.service';
+
+@Resolver()
+export class TodoResolver {
+  constructor(
+    private readonly todoService: TodoService
+  ) { }
+
+  @Query(() => [Todo], { name: "todos" })
+  findAll(): Todo[] {
+    return this.todoService.findAll();
+  }
+
+  @Query(() => Todo, { name: "todo" })
+  findOne(
+    @Args('id', { type: () => Int }) id: number
+  ): Todo {
+    return this.todoService.findOne(id);
+  }
+
+  create() {
+    return {};
+  }
+
+  update() {
+    return {};
+  }
+
+  remove() {
+    return {};
+  }
+}
